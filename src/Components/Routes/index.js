cdd148d17528ff121b2d2d53442dac1b4f43a824
hugomaxbr <hugofranco_12@hotmail.com>
2021-03-28 17:13:07 -0300

    prototype login route added

--- src/Components/Routes/index.js
+++ src/Components/Routes/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from '../../Pages/Login';
 import Home from '../../Pages/Home';
 import Register from '../../Pages/Register';
+import Login2 from '../../Pages/Login2';
 
 
 
@@ -14,6 +15,7 @@ function Routes() {
         <Route exact path="/" component={Home} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
+        <Route path="/login2" component={Login2} />
       </Switch>
     </BrowserRouter>
   );
