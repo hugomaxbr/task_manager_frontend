15019f75f3cd661247be04c016ab8ad6425e4b20
Hugo Franco Alves Gil <hugo.alves@coopercitrus.com.br>
2021-03-30 11:30:11 -0300

    new Font added

--- src/Styles/GlobalStyle.js
+++ src/Styles/GlobalStyle.js
@@ -1,11 +1,14 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap');
+  
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: Roboto;
+    font-family: 'Share Tech Mono', monospace;
   }
 
   body {
