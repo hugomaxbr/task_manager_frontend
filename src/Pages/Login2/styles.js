aa46716b0f734afcbf708872cf4b4552744ad274
Hugo Franco Alves Gil <hugo.alves@coopercitrus.com.br>
2021-03-30 11:29:55 -0300

    style: some style changes have been made

--- src/Pages/Login2/styles.js
+++ src/Pages/Login2/styles.js
@@ -3,10 +3,19 @@ import color from '../../Styles/Colors';
 import afterScreen from './afterScreen.png';
 import verLinesLeft from './verLinesLeft.png';
 import verLinesRight from './verLinesRight.png';
+import horDots from './horDots.png';
+import smallLines from './smallLines.png'
+import tiltedLines from './tiltedLines.png'
+
+export const Envelocro = styled.div`
+    max-width: 1500px;
+    height: 100%;
+    position: relative;
+`;
 
 
 export const Container = styled.div`
-    background-color: ${color.smokedBlack};
+    background-color: black;
     left: 0;
     right: 0;
     position: absolute;
@@ -19,10 +28,10 @@ export const Container = styled.div`
         content: "";
         display: block;
         background: url(${afterScreen}) no-repeat 50%;;
-        margin: 0 auto;
+        margin: -50px auto;
         width: 100%;
         max-width: 500px;
-        height: 28px;
+        height: 52px;
     }
 
 
@@ -31,11 +40,44 @@ export const Container = styled.div`
 
 
 export const Screen = styled.div`
-    margin: 105px  auto;
-    background: ${color.vibrantYellow};
-    width: 85%;
-    height: 70%;
-    position : relative;
+    position: relative;
+    margin: 0 auto;
+    width: 90%;
+    max-width: 1080px;
+    height: 488px;
+    border-top: 18px solid #000;
+    border-bottom: 10px solid #000;
+    border-image: url(${horDots}) 17 11 17 round;
+    display: block;
+    margin: 0 58.406;
+
+    &::after{
+        left: -73px;
+        content: "";
+        position: absolute;
+        background: url(${smallLines}) no-repeat 50%;
+        width: 73px;
+        height: 460px;
+    }
+    &::before{
+        right: -73px;
+        content: "";
+        position: absolute;
+        background: url(${smallLines}) no-repeat 50%;
+        width: 73px;
+        height: 460px;
+    }
+    
+ 
+`; 
+
+
+
+export const InsideScreen = styled.div`
+    margin: 0 auto;
+    width: 100%;
+    height: 100%;
+    position : absolute;
 
     &::after {
         content: "";
@@ -63,6 +105,29 @@ export const Screen = styled.div`
     
 `;
 
+export const InnerBox = styled.div`
+
+    display: inline-block;
+    width: calc(100% - 70px);
+    max-width: 1010px;
+    text-transform: uppercase;
+    text-align: center;
+
+    &::before{
+        content: "";
+        display: inline-block;
+        background: url(${tiltedLines}) no-repeat 50%;
+        max-width: 642px;
+        width: 100%;
+        height: 42px;
+    }
+
+    
+    
+    
+`;
+
+
 export const Titulo = styled.h3`
     &::before{
         width: 108px;
@@ -71,15 +136,17 @@ export const Titulo = styled.h3`
         margin: 0 8px;
         content: "";
         display: inline-block;
-        background: ${color.vibrantYellow}
+        background: #db0e15;
         
     
     }
     text-transform: uppercase;
     text-align: center;
-    font-size: 1em;
+    color: #db0e15;
+    margin: 25px 0;
+    font-size: 26px;
     font-weight: 300;
-    color: ${color.vibrantYellow};
+    text-shadow: 0 0 5px rgb(219 14 21 / 80%);
 
     &::after{
         width: 108px;
@@ -88,6 +155,6 @@ export const Titulo = styled.h3`
         margin: 0 8px;
         content: "";
         display: inline-block;
-        background: ${color.vibrantYellow}
+        background: #db0e15;
     }
 `;
